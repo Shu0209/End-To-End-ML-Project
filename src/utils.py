e3e6724c098dad0793308c22ce0c6e350fcbafd4
Shubham926 <bhandarishubham926@gmail.com>
2026-05-25 01:53:27 +0530

    Add Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,9 @@ import dill
 import numpy as np
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
 from src.logger import logging
 
@@ -21,3 +24,26 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def evaluate_models(x_train,y_train,x_test,y_test,models):
+    try:
+
+        report={}
+
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            model.fit(x_train,y_train)
+
+            y_train_predict=model.predict(x_train)
+            y_test_predict=model.predict(x_test)
+
+            train_model_score=r2_score(y_train,y_train_predict)
+            test_model_score=r2_score(y_test,y_test_predict)
+
+            report[list(models.keys())[i]]=test_model_score
+
+        return report
+
+    except:
+        pass
